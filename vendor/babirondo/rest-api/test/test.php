8482bbf8cb8c55c9256c08a8838f08c7ce0b8856
Bruno Siqueira <babirondo@gmail.com>
2018-12-20 23:43:56 +0000

    servico de registro de imagens funcionando com mongo. entidade jogadores integrada a api de imagens. mensageria e workers funcionando. workers adicionado ao startdocker. falta criar o pipeline de deploy no jenkins com webhook

--- vendor/babirondo/rest-api/test/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
-use REST\RESTCall;
-
-$API = new RESTCall();
-//echo RESTCall::CallAPI(null, null);
-echo "foi";
-//echo RESTCall::CallAPI(null,null);
