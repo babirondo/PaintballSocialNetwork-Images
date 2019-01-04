e779895bc3f93b0ee2ff2bbf3f7df444cef49cce
bruno <babirondo@gmail.com>
2019-01-04 17:52:29 +0000

    Service Imagens redimensionando imagens postadas e Meu PErfil ja exibindo a imagem tratada sem blobs!

--- include/class_images.php
+++ include/class_images.php
@@ -10,6 +10,7 @@ class Images{
         include "vendor/autoload.php";
 
         require_once("include/globais.php");
+        require_once("include/SimpleImage.php");
         $this->Globais = new Globais();
 
         $this->con = new \babirondo\classbd\db();
@@ -23,33 +24,105 @@ class Images{
 
         $this->con->MongoDB = $this->Globais->MongoConf["Index"];
         $this->con->MongoTable = $MongoTable;//$this->Globais->Championship["Type"]["campeonato"];
+
+        $this->folder  = __DIR__."/../generated_images/";
+    }
+
+    function base64_to_jpeg($base64_string, $output_file) {
+
+      //echo "\n\n\n\n\n PASTA ".$pasta_salvar_imagens.$output_file;
+        // open the output file for writing
+        $ifp = fopen( $this->folder.$output_file, 'wb' );
+
+        // split the string on commas
+        // $data[ 0 ] == "data:image/png;base64"
+        // $data[ 1 ] == <actual base64 string>
+        $data = explode( ',', $base64_string );
+
+        // we could add validation here with ensuring count( $data ) > 1
+        fwrite( $ifp, base64_decode( $data[ 1 ] ) );
+
+        // clean up the file resource
+        fclose( $ifp );
+
+        return $output_file;
     }
 
 
+        function resize_image($imagem, $tipo, $idusuario=""){
+          switch($tipo){
+            case("Profile"):
+              $altura = 320 ;
+              $largura = 320;
+            break;
+          }
+          $nome_arquivo_foto = "$idusuario-original-".uniqid().".jpg";
+          $this->base64_to_jpeg($imagem,$nome_arquivo_foto );
+          $nome_arquivo_foto_resized = "$idusuario-$tipo-".uniqid().".jpg";
+
+          $image = new SimpleImage();
+          if (! $image->load( $this->folder.$nome_arquivo_foto ) ){
+            echo "Nao foi possivel ler os atributos da imagem";
+
+            return false;
+          }
+          else{
+
+            if ($image->getHeight > $image->getWidth){
+              $image->resizeToHeight( $altura);
+            }
+            else {
+              $image->resizeToWidth( $largura);
+            }
+            $image->save($this->folder.$nome_arquivo_foto_resized);
+
+            return $nome_arquivo_foto_resized;
+          }
+        }
+
+
+
+        function store_uploaded_image($html_element_name, $new_img_width, $new_img_height) {
+
+            $target_dir = "your-uploaded-images-folder/";
+            $target_file = $target_dir . basename($_FILES[$html_element_name]["name"]);
+
+            return $target_file; //return name of saved file in case you want to store it in you database or show confirmation message to user
+
+        }
+
         function setImage(  $jsonRAW , $args ){
 
-            if (!$this->con->conectado){
+            if (!$this->con->conectado || !$jsonRAW["imagem"]){
               return false;
-
             }
 
             IF (!$jsonRAW  ) {
               return false;
             }
             $jsonRAW["IDUSUARIO"] = $args["idusuario"];
-            $resultMongo = $this->con->MongoInsertOne( $jsonRAW );
-            $idMongo = $resultMongo->getInsertedId();
 
-            $data["msg"] = "Inserted with Object ID '{$idMongo}'";
 
-            if ( $idMongo ){
-                return $idMongo;
+
+            $imagem_redimensionada = $this->resize_image($jsonRAW["imagem"], $jsonRAW["TipoImagem"], $args["idusuario"]);
+            if ($imagem_redimensionada != false){
+              $jsonRAW["imagem"] =   $imagem_redimensionada;
+
+              $resultMongo = $this->con->MongoInsertOne( $jsonRAW );
+              $idMongo = $resultMongo->getInsertedId();
+
+              $data["msg"] = "Inserted with Object ID '{$idMongo}'";
+
+              if ( $idMongo ){
+                  return $idMongo;
+              }
+              else {
+                  // nao encontrado
+                  return false;
+              }
             }
-            else {
-                // nao encontrado
-                return false;
-            }
-
+            else
+              return false;
         }
 
     function getImage (  $args ){
--- tests/testImages.php
+++ tests/testImages.php
@@ -213,13 +213,13 @@ class testImages extends PHPUnit\Framework\TestCase
 
 
       if ($this->SaveConf($Conf) == 0){
-        echo " Nao foipossivel salvar o arqvuio de conf";
+        echo " Nao foi possivel salvar o arqvuio de conf";
         exit;
       }
 
       $this->assertEquals('SUCESSO', $jsonRetorno["resultado"] );
   }
-
+/*
 
   public function testDELETE_DeletePlayerImage()
  {
@@ -259,105 +259,7 @@ class testImages extends PHPUnit\Framework\TestCase
 
      $this->assertEquals('SUCESSO', $jsonRetorno["resultado"] );
  }
-/*
-    public function testPUT_AlterandoMeuTime()
-    {
-
-        set_time_limit(10);
-        $Conf = $this->OpenConf();
-        $idjogador = 10;
-
-        $time = "testAAA meu novo time ;) ALTERADO ".rand(500,8500);
-        $idtime= $Conf["idtime"];
-
-        $JSON = json_decode( " {\"time\":\"$time\",\"treino\":{\"Domingo\":\"Domingo\"},\"nivelcompeticao\":\"D2\",\"procurando\":{\"Doritos\":\"Doritos\"},\"localtreino\":\"Dublin\",\"foto\":{\"name\":\"\",\"type\":\"\",\"tmp_name\":\"\",\"error\":4,\"size\":0},\"idtime\":\"$idtime\"} " , true);
-        if ($JSON == NULL ) die(" JSON erro de formacao");
-
-        $trans = null;$trans = array(":idjogadorlogado" => $idjogador );
-        //var_dump(strtr($this->Globais->MeusTimesRemoto, $trans));
-
-        $response = $this->client->request('PUT', strtr($this->Globais->CriarMeuTimeSalvar, $trans)
-
-            , array(
-                'headers' => array('Content-type' => 'application/x-www-form-urlencoded'),
-                'timeout' => 10, // Response timeout
-                'form_params' => $JSON,
-                'connect_timeout' => 10 ,// Connection timeout,
-           )
-        );
-        $jsonRetorno = json_decode($response->getBody()->getContents(), 1);
-
-        $Conf = $this->OpenConf();
-        $Conf["time"] = $time;
-        if ($this->SaveConf($Conf) == 0){
-          echo " Nao foipossivel salvar o arqvuio de conf";
-          exit;
-        }
-
-
-
-
-        $this->assertEquals('SUCESSO', $jsonRetorno["resultado"] );
-    }
-
-    public function testPOST_ProcurarTimes()
-    {
-        $Conf = $this->OpenConf();
-        set_time_limit(10);
-        $idjogador = 10;
-
-        $JSON = json_decode( " {\"time\":\"".$Conf["time"]."\",\"treino\":null,\"localtreino\":\"\",\"nivelcompeticao\":\"\",\"procurando\":null,\"filtro\":1} " , true);
-
-        $trans = null;$trans = array(":idjogadorlogado" => $idjogador );
-        //var_dump(strtr($this->Globais->ProcurarTimes, $trans)); var_dump($JSON);
-
-        $response = $this->client->request('POST', strtr($this->Globais->ProcurarTimes, $trans)
-
-            , array(
-                'headers' => array('Content-type' => 'application/x-www-form-urlencoded'),
-                'timeout' => 10, // Response timeout
-                'form_params' => $JSON,
-                'connect_timeout' => 10 // Connection timeout
-
-
-            )
-        );
-        $jsonRetorno = json_decode($response->getBody()->getContents(), 1);
-
-        //   var_dump($jsonRetorno);
-        $this->assertEquals('SUCESSO', $jsonRetorno["resultado"] );
-    }
-
-    public function testGET_MeusTimes()
-    {
-
-        set_time_limit(10);
-        $idjogador = 10;
-
-
-        $JSON = json_decode( " {\"nome\":\"\",\"treino\":null,\"localtreino\":\"\",\"nivelcompeticao\":\"\",\"procurando\":null,\"filtro\":1} " , true);
-
-        $trans = null;$trans = array(":idjogadorlogado" => $idjogador );
-        //var_dump(strtr($this->Globais->MeusTimesRemoto, $trans));
-
-        $response = $this->client->request('GET', strtr($this->Globais->MeusTimesRemoto, $trans)
-
-            , array(
-                'headers' => array('Content-type' => 'application/x-www-form-urlencoded'),
-                'timeout' => 10, // Response timeout
-                'form_params' => $JSON,
-                'connect_timeout' => 10 // Connection timeout
-
-
-            )
-        );
-        $jsonRetorno = json_decode($response->getBody()->getContents(), 1);
-
-        //   var_dump($jsonRetorno);
-        $this->assertEquals('SUCESSO', $jsonRetorno["resultado"] );
-    }
 */
-
 
 
 }
